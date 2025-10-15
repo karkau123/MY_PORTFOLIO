f1c66c81ed43a5feeda6fde9d978a42b59d8c2a7
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2025-10-16 00:43:26 +0530

    portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,341 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "E-commerce Platform",
+    description:
+      "",
+    subDescription: [
+      "Built FastAPI endpoints with SQLAlchemy and PostgreSQL for scalable product, user, and order management.",
+      "Developed a fast, responsive React SPA (Single-Page Application) using Redux for state management and Tailwind CSS for modern UI.",
+      "Integrated a robust and secure payment gateway ensuring seamless and reliable transaction handling.",
+      "Optimized database interactions and implemented caching strategies to deliver high-performance, low-latency data retrieval",
+    ],
+    href: "https://github.com/karkau123/Scalable-Ecommerce-app",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: "Fastapi",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 3,
+        name: "React",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 4,
+        name: "Redux",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "SQLAlchemy",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "PostgreSQL",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "Redis",
+        path: "/assets/logos/tailwindcss.svg",
+      }
+      
+    ],
+  },
+  {
+    id: 2,
+    title: "PrepBuddy – CSE Placement Prep Tool",
+    description:
+      "A comprehensive platform designed to help computer science students prepare for placements with interactive features and AI assistance.",
+    subDescription: [
+      "Built a full-stack platform for CSE placement prep with subject-wise progress tracking and interactive mind maps.",
+      "Integrated Google Gemini AI Key to deliver context-aware answers for subtopic-specific and conceptual queries.",
+      "Implemented secure user authentication using Firebase (Email/Password + Google Sign-In).",
+      "Added theme toggle and responsive UI for a personalized, mobile-friendly experience.",
+    ],
+    href: "https://github.com/karkau123/Website-Project?tab=readme-ov-file",
+    logo: "",
+    image: "/assets/projects/auth-system.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "Firebase",
+        path: "/assets/logos/auth0.svg",
+      },
+      {
+        id: 4,
+        name: "Firestore",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 5,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 6,
+        name: "Generative AI",
+        path: "/assets/logos/react.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "CPU and Memory Scheduling Visualizer",
+    description:
+      "An interactive application that visualizes various CPU and Memory scheduling algorithms to help understand process management concepts.",
+    subDescription: [
+      "Built a PC application using Flutter that visualizes scheduling algorithms for CPU and Memory.",
+      "Developed visualizations for CPU scheduling algorithms (FCFS, SJF, RR) to enhance understanding of process management.",
+      "Implemented memory scheduling visualizations with multiple allocation strategies, including Best Fit.",
+      "Created an intuitive user interface for better learning experience of operating system concepts.",
+    ],
+    href: "https://github.com/karkau123/Scheduler",
+    logo: "",
+    image: "/assets/projects/blazor-app.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Flutter",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Dart",
+        path: "/assets/logos/dotnetcore.svg",
+      }
+    ],
+  },
+  {
+    id: 4,
+    title: "Zwigato",
+    description:
+      "A feature-rich food delivery application built with Flutter, offering a seamless ordering experience with real-time location tracking.",
+    subDescription: [
+      "Developed a food delivery application with a clean, responsive UI using Firebase for backend services.",
+      "Implemented themes such as dark and light modes and added delivery animations for an enhanced user experience.",
+      "Integrated authentication logic and Firestore DB to store user orders and payment receipts.",
+      "Introduced features such as a side drawer and bottom tabs for easy filtering of different meals and food categories.",
+      "Enabled users to add an address or fetch their current location using Google Maps API.",
+    ],
+    href: "https://github.com/karkau123/Zwigato",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Flutter",
+        path: "/assets/logos/cplusplus.svg",
+      },
+      {
+        id: 2,
+        name: "Dart",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Firebase",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "Firestore",
+        path: "/assets/logos/microsoft.svg",
+      },
+      {
+        id: 5,
+        name: "Google Maps API",
+        path: "/assets/logos/microsoft.svg",
+      }
+    ],
+  },
+  /*{
+    id: 5,
+    title: "WordPress Custom Theme",
+    description:
+      "A fully customizable WordPress theme optimized for performance and SEO.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/wordpress-theme.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "WordPress",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "Vite.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Online Learning Platform",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built using Blazor WebAssembly for a seamless SPA experience.",
+      "Implemented video streaming with Azure Media Services.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/elearning.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Blazor",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Azure",
+        path: "/assets/logos/azure.svg",
+      },
+      {
+        id: 3,
+        name: "Stripe",
+        path: "/assets/logos/stripe.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },*/
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ali-sanati/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Backend Development Intern",
+    job: "TATA 1MG",
+    date: "Jan–Aug 2025",
+    contents: [
+      "Tools & Technologies: Python, FastAPI, Sanic",
+      "Architected scalable, high-throughput microservice ecosystem using FastAPI and Sanic with async I/O, enabling modular API endpoints and seamless scalability under load.",
+      "Integrated Spike API, replacing existing Terra API to stream wearable health data—saving 1mg nearly $4200 annualy",
+      "Designed and developed low-latency APIs with Pydantic validation, OAuth-based authentication, and efficient schema normalization to support fast and reliable user insights.",
+      "Optimized existing APIs using techniques like caching, response compression, and query optimization, reducing average latency by 10–15.%",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    job: "Indiamart",
+    date: "Sep 2025-Present",
+    contents: [
+      "Tools & Technologies: Golang, Docker, Docker Swarm, Docker Compose, NVIDIA CUDA",
+      "Migrated machine learning model deployment from a GKE CPU-based infrastructure to a GPU-based VM environment with NVIDIA Tesla T4 GPUs to accelerate processing.",
+      "Utilized Docker, Docker Swarm, and Docker Compose to containerize and orchestrate the new deployment.",
+      "Achieved a drastic reduction in API latency, from an average of 38ms down to 7-8ms.",
+      "Currently developing a sales-based agent to automate and reduce the workload for sellers on the platform.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Leetcode",
+    username: "@jack",
+    body: " Rated 1721 at LeetCode with over 850 problems solved, ranking among the top 10% of Coders",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Codechef",
+    username: "@jill",
+    body: " Reached a peak rating of 1720 (3-star) on CodeChef with a global rank of 520 in the Starters 120 Competition.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "HackNITP 2.0",
+    username: "@john",
+    body: "Secured 1st Rank in HackNITP 2.0 Hackathon, showcasing coding proficiency and critical thinking skills.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Team Lead",
+    username: "@Backend",
+    body: "Google Developer Student Club – Backend Team",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Academics",
+    username: "@bob",
+    body: "Secured a perfect 10.0 CGPA in the 8th semester, showcasing strong academic excellence.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Hackathon Tata 1mg",
+    username: "@charlie",
+    body: "Runner up",
+    img: "https://robohash.org/charlie",
+  },
+  // {
+  //   name: "Dave",
+  //   username: "@dave",
+  //   body: "Simply breathtaking. The best decision I've made in a while.",
+  //   img: "https://robohash.org/dave",
+  // },
+  // {
+  //   name: "Eve",
+  //   username: "@eve",
+  //   body: "So glad I found this. It has changed the game for me.",
+  //   img: "https://robohash.org/eve",
+  // },
+];
